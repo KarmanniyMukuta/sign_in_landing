5ea99001c8fa3a53c658ec9c86b1d93aecef113c
KarmanniyMukuta <gen.nefchev@gmail.com>
2023-01-19 23:43:49 +0200

    DONE

---
+++ script.js
@@ -0,0 +1,9 @@
+function show_hide_password(target){
+	var input = document.getElementById('password-input');
+	if (input.getAttribute('type') == 'password') {
+		input.setAttribute('type', 'text');
+	} else {
+		input.setAttribute('type', 'password');
+	}
+	return false;
+}
